36bbb384c0ab97382a563b564f92fde4dc60e2a5
Han Hossain <ahanuf12@gmail.com>
2020-10-24 18:44:03 -0700

    make it compile

---
+++ chakracore-sys/build.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // link ChakraCore shared lib
+    println!("cargo:rustc-link-lib=ChakraCore");
+
+    // invalidate the built crate when the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
