1238f189f842c25ee77c35aea509ce4247486c5c
Han Hossain <ahanuf12@gmail.com>
2020-10-30 22:24:26 -0700

    basic logging test

--- chakracore-sys/src/lib.rs
+++ chakracore-sys/src/lib.rs
@@ -8,8 +8,11 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 mod tests {
     use super::*;
     use std::convert::TryInto;
-    use std::ffi::{CStr, CString};
+    use std::ffi::{c_void, CStr, CString};
+    use std::os::raw::c_ushort;
     use std::ptr;
+
+    static mut DONE: bool = false;
 
     fn assert_no_error(error_code: _JsErrorCode) {
         assert_eq!(error_code, _JsErrorCode_JsNoError);
@@ -100,6 +103,111 @@ mod tests {
             // dispose runtime
             let res = JsDisposeRuntime(runtime);
             assert_no_error(res);
+        }
+    }
+
+    unsafe extern "C" fn log(
+        _callee: JsValueRef,
+        _isConstructCall: bool,
+        _arguments: *mut JsValueRef,
+        _argumentCount: c_ushort,
+        _callbackState: *mut c_void,
+    ) -> JsValueRef {
+        DONE = true;
+        ptr::null_mut()
+    }
+
+    #[test]
+    fn basic_logging() {
+        let script = "(() => { console.log('hello world'); })()";
+        let script_c = CString::new(script).unwrap().into_raw();
+
+        unsafe {
+            let mut runtime: JsRuntimeHandle = ptr::null_mut();
+            let mut context: JsContextRef = ptr::null_mut();
+
+            // Create a runtime.
+            let res = JsCreateRuntime(
+                _JsRuntimeAttributes_JsRuntimeAttributeNone,
+                None,
+                &mut runtime,
+            );
+            assert_no_error(res);
+
+            // Create an execution context.
+            let res = JsCreateContext(runtime, &mut context);
+            assert_no_error(res);
+
+            // Now set the current execution context.
+            let res = JsSetCurrentContext(context);
+            assert_no_error(res);
+
+            // create console object
+            let mut console = ptr::null_mut();
+            assert_no_error(JsCreateObject(&mut console));
+
+            // create log function
+            let mut log_func = ptr::null_mut();
+            assert_no_error(JsCreateFunction(Some(log), ptr::null_mut(), &mut log_func));
+            let log_string = CString::new("log").unwrap();
+            let mut log_prop_id = ptr::null_mut();
+            assert_no_error(JsCreatePropertyId(
+                log_string.as_ptr(),
+                log_string.as_bytes().len() as u64,
+                &mut log_prop_id,
+            ));
+            assert_no_error(JsSetProperty(console, log_prop_id, log_func, true));
+
+            // set console as property of global object
+            let mut global = ptr::null_mut();
+            assert_no_error(JsGetGlobalObject(&mut global));
+            let console_string = CString::new("console").unwrap();
+            let mut console_prop_id = ptr::null_mut();
+            assert_no_error(JsCreatePropertyId(
+                console_string.as_ptr(),
+                console_string.as_bytes().len() as u64,
+                &mut console_prop_id,
+            ));
+            assert_no_error(JsSetProperty(global, console_prop_id, console, true));
+
+            let mut fname = ptr::null_mut();
+            let sample = CString::new("sample").unwrap();
+            let res = JsCreateString(sample.as_ptr(), 6, &mut fname);
+            assert_no_error(res);
+
+            let mut script_source = ptr::null_mut();
+            let res = JsCreateExternalArrayBuffer(
+                script_c as *mut _,
+                script.len() as u32,
+                None,
+                ptr::null_mut(),
+                &mut script_source,
+            );
+            assert_no_error(res);
+
+            // run the script
+            let mut result = ptr::null_mut();
+            let res = JsRun(
+                script_source as *mut _,
+                0usize,
+                fname as *mut _,
+                _JsParseScriptAttributes_JsParseScriptAttributeNone,
+                &mut result,
+            );
+            assert_no_error(res);
+
+            // release script_c memory
+            let _ = CString::from_raw(script_c);
+
+            // clear current context
+            let res = JsSetCurrentContext(ptr::null_mut());
+            assert_no_error(res);
+
+            // dispose runtime
+            let res = JsDisposeRuntime(runtime);
+            assert_no_error(res);
+
+            assert!(DONE);
         }
     }
 }
