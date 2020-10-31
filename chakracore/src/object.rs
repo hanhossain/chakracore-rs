ca733ecdd23c9b2e8fb9cb6ea2abc0edc177ee9f
Han Hossain <ahanuf12@gmail.com>
2020-10-30 22:48:10 -0700

    JsObject::global()

--- chakracore/src/object.rs
+++ chakracore/src/object.rs
@@ -1,5 +1,5 @@
 use crate::error::JsError;
-use chakracore_sys::{JsCreateObject, JsValueRef};
+use chakracore_sys::{JsCreateObject, JsGetGlobalObject, JsValueRef};
 use std::ptr;
 
 #[derive(Debug)]
@@ -11,6 +11,14 @@ impl JsObject {
     pub fn new() -> Result<Self, JsError> {
         let mut result = ptr::null_mut();
         let res = unsafe { JsCreateObject(&mut result) };
+        JsError::assert(res)?;
+
+        Ok(Self { handle: result })
+    }
+
+    pub fn global() -> Result<Self, JsError> {
+        let mut result = ptr::null_mut();
+        let res = unsafe { JsGetGlobalObject(&mut result) };
         JsError::assert(res)?;
 
         Ok(Self { handle: result })
@@ -30,6 +38,16 @@ mod tests {
         context.set_current_context().unwrap();
 
         let object = JsObject::new().unwrap();
+        assert!(!object.handle.is_null());
+    }
+
+    #[test]
+    fn get_global_object() {
+        let mut runtime = JsRuntime::new(JsRuntimeAttributes::None).unwrap();
+        let mut context = JsScriptContext::new(&mut runtime).unwrap();
+        context.set_current_context().unwrap();
+
+        let object = JsObject::global().unwrap();
         assert!(!object.handle.is_null());
     }
 }
